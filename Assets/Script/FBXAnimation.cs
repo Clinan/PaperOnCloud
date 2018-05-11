4132412242c136ffb7538c9d2383e2e6c40dedd7
clan <artergo163@163.com>
2018-05-11 20:31:05 +0800

    添加小猪佩奇的场景,修复了两个bug

---
+++ Assets/Script/FBXAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBXAnimation : MonoBehaviour {
+
+    private Animation animation;
+    public string animationName;
+    public float m_speed = 1.0f;
+    void Start()
+    {
+        animation = GetComponent<Animation>();//动画控制器  
+        animation.Play(animationName);//上来直接播放idle动画  
+
+        foreach (AnimationState state in animation)
+        {
+            state.speed = m_speed;
+        }
+
+    }
+    private void Update()
+    {
+
+        if (!animation.isPlaying)
+        {
+            animation.Play(animationName);
+
+        }
+    }
+   
+}
--- Assets/Script/LoadCustomeScene.cs
+++ Assets/Script/LoadCustomeScene.cs
@@ -10,49 +10,26 @@ public class LoadCustomeScene : MonoBehaviour
 
     public string sceneName = "first";
 
-    public Canvas optionCanvas;
-    public Button okButton;
-    public Button cancelButton;
-    public bool flag = true;
+    private Canvas optionCanvas;
+    private Button okButton;
+    private bool flag = true;
 
-    // Use this for initialization
     void Start()
     {
-        //DestroyObject(optionCanvas);
-
-        //if (Application.platform == RuntimePlatform.Android)
-        //{
-        //    AndroidJavaClass jc = new AndroidJavaClass("com.quyue.paperoncloud.UnityActivity");
-        //    AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("mActivity");
-        //    string[] mObject = new string[2];
-        //    bool result = jo.Call<bool>("showDialog", mObject);
-        //    if (result)
-        //    {
-        //        SceneManager.LoadScene(sceneName);
-
-        //    }
-        //}
-        //else
-        //{
         Debug.Log("start load");
-            optionCanvas = Instantiate(optionCanvas);
-            Button[] bs = optionCanvas.GetComponentsInChildren<Button>();
-            foreach (Button b in bs)
+        optionCanvas = gameObject.GetComponentInChildren<Canvas>();
+        Button[] bs = optionCanvas.GetComponentsInChildren<Button>();
+        foreach (Button b in bs)
+        {
+            if (b.gameObject.name.Equals("btn_ok"))
             {
-                if (b.gameObject.name.Equals("btn_ok"))
-                {
-                    okButton = b;
-                }
-                if (b.gameObject.name.Equals("btn_cancel"))
-                {
-                    cancelButton = b;
-                }
-
+                okButton = b;
             }
 
+        }
+
         okButton.onClick.AddListener(OkOnClick);
-        cancelButton.onClick.AddListener(CancelOnClick);
-        //}
+
     }
 
     // Update is called once per frame
@@ -64,27 +41,11 @@ public class LoadCustomeScene : MonoBehaviour
 
     public void OkOnClick()
     {
-        
-            Debug.Log("ok");
-            optionCanvas.gameObject.SetActive(false);
-            //DestroyImmediate(optionCanvas, true);
-            //Component[] component = optionCanvas.GetComponentsInChildren<Component>();
-            //foreach (Component c in component)
-            //{
-            //    DestroyImmediate(c, true);
-            //}
-            Scene first = SceneManager.GetSceneByName("first");
-            SceneManager.UnloadSceneAsync(first);
-            SceneManager.LoadScene(sceneName);
-        
-        
-        
-    }
 
-    public void CancelOnClick()
-    {
+        Debug.Log("ok");
+        SceneManager.LoadScene(sceneName);
 
-        optionCanvas.gameObject.SetActive(false);
+
     }
 
 
